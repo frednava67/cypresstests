c4d19f7e04228b7c9787b333b331010be937f490
Fred Nava <frednava67@hotmail.com>
2018-02-20 21:13:21 -0800

    Added first set of click navigation cases

--- cypress/integration/Hulu Press Footer Tests.js
+++ cypress/integration/Hulu Press Footer Tests.js
@@ -1,5 +1,8 @@
 const strSiteURL = 'https://www.hulu.com/press';
 const slctrFooterUL = '#page-44 > footer > nav > ul';
+
+
+
 const slctrNav = '#page-44 > header > div.regular-menu > nav';
 
 describe('Hulu Footer Existence Test', function() {
@@ -65,4 +68,157 @@ describe('Hulu Footer Existence Test', function() {
         cy.get(slctrFooterUL).contains('2017 Hulu')
   
       })
+})
+
+describe('Hulu Footer Click Navigation Tests', function() {
+
+  context('Hulu Section', function () {
+
+    beforeEach(function () {
+      cy.visit(strSiteURL)
+      cy.title().should('include', 'Hulu Press Site')
+    })
+
+    // Click 'About Us' item
+    //
+    it('Validates the Hulu Press About Us Footer item in the Hulu section activation/navigation', function() {
+      cy
+        .get(slctrFooterUL)
+        .contains('About Us')
+        .click()
+        .title()
+        .should('include','About Hulu – Hulu Press Site')
+    })
+
+    // Click 'Executives' item
+    //
+    it('Validates the Hulu Press Executives Footer item in the Hulu section activation/navigation', function() {
+      cy
+        .get(slctrFooterUL)
+        .contains('Executives')
+        .click()
+        .title()
+        .should('include','Executive Team – Hulu Press Site')
+    })
+
+    // Click 'Subscribe Now' item
+    //
+    it('Validates the Hulu Press Subscribe Now Footer item in the Hulu section activation/navigation', function() {
+      cy
+        .get(slctrFooterUL)
+        .contains('Subscribe Now')
+        .click()
+        .title()
+        .should('include','TV Shows and Movies - Watch Your Favorite TV Episodes and Movies Online | Hulu')
+    })
+  })
+
+  context('Advertising Section', function () {
+
+    beforeEach(function () {
+      cy.visit(strSiteURL)
+      cy.title().should('include', 'Hulu Press Site')
+    })
+
+    // Click 'Ad Experience' item
+    //
+    it('Validates the Hulu Press Ad Experience Footer item in the Advertising section activation/navigation', function() {
+      cy
+        .get(slctrFooterUL)
+        .contains('Ad Experience')
+        .click()
+        .title()
+        .should('include','Hulu Advertising')
+    })
+
+    // Click 'Brand Solutions' item
+    //
+    it('Validates the Hulu Press Brand Solutions Footer item in the Advertising section activation/navigation', function() {
+      cy
+        .get(slctrFooterUL)
+        .contains('Brand Solutions')
+        .click()
+        .title()
+        .should('include','Brand Solutions – Hulu Advertising')
+    })
+
+    // Click 'Hulu Originals' item
+    //
+    it('Validates the Hulu Press Hulu Originals Footer item in the Advertising section activation/navigation', function() {
+      cy
+        .get(slctrFooterUL)
+        .contains('Hulu Originals')
+        .click()
+        .title()
+        .should('include','Hulu Originals – Hulu Advertising')
+    })
+    
+    // Click 'Description of Methodology (DOM)' item
+    //
+    it('Validates the Hulu Press Description of Methodology Footer item in the Advertising section activation/navigation', function() {
+      cy
+        .get(slctrFooterUL)
+        .contains('Description of Methodology')
+        .click('top')
+        .title()
+        .should('include','Description of Methodology (DOM) – Hulu Advertising')
+    })
+
+
+
+  })
+
+  context.only('Jobs Section', function () {
+
+    beforeEach(function () {
+      cy.visit(strSiteURL)
+      cy.title().should('include', 'Hulu Press Site')
+    })
+
+    // Click 'Positions' item
+    //
+    it('Validates the Hulu Press Positions Footer item in the Jobs section activation/navigation', function() {
+      cy
+        .get(slctrFooterUL)
+        .contains('Positions')
+        .click()
+        .title()
+        .should('include','Hulu Jobs')
+    })
+
+    // Click 'Hulugan Life' item
+    //
+    it('Validates the Hulu Press Hulugan Life Footer item in the Jobs section activation/navigation', function() {
+      cy
+        .get(slctrFooterUL)
+        .contains('Hulugan Life')
+        .click()
+        .title()
+        .should('include','Hulu Jobs')
+    })
+
+    // Click 'What Defines Hulu' item
+    //
+    it('Validates the Hulu Press What Defines Hulu Footer item in the Jobs section activation/navigation', function() {
+      cy
+        .get(slctrFooterUL)
+        .contains('What Defines Hulu')
+        .click()
+        .title()
+        .should('include','Hulu Jobs')
+    })
+    
+    // // Click 'Employee Verification' item
+    // //
+    // it('Validates the Hulu Press Employee Verification Footer item in the Jobs section activation/navigation', function() {
+    //   cy
+    //     .get(slctrFooterUL)
+    //     .contains('Employee Verification')
+    //     .click('top')
+    //     .title()
+    //     .should('include','Description of Methodology (DOM) – Hulu Advertising')
+    // })
+
+  })
+
 })
