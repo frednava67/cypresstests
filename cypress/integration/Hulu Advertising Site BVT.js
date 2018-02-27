0b2d5e256862ad06a0e0e74ed129582edc0a4385
frednava67 <frednava67@hotmail.com>
2018-02-26 21:40:29 -0800

    refactored original test config and  started adding page element tests

--- cypress/integration/Hulu Advertising Site BVT.js
+++ cypress/integration/Hulu Advertising Site BVT.js
@@ -1,41 +1,77 @@
-describe('Advertising Site Launch Test', function() {
+const strSiteURL = 'https://www.hulu.com/advertising';
+
+describe('Advertising Site URL Launch Tests', function() {
+
     it('Visits the Hulu Advertising area', function() {
         cy.visit('https://www.hulu.com/advertising/')
         cy.title().should('include', 'Hulu Advertising')
     })
-})
 
-describe('Ad Experiences Launch Test', function() {
-    it('Visits the Ad Experience area', function() {
+    it('Opens the Ad Experience area', function() {
         cy.visit('https://www.hulu.com/advertising/ad-experiences/')
         cy.title().should('include', 'Ad Experience – Hulu Advertising')
     })
-})
 
-describe('Brand Solutions Launch Test', function() {
-    it('Visits the Brand Solutions area', function() {
+    it('Opens the Brand Solutions area', function() {
         cy.visit('https://www.hulu.com/advertising/brand-solutions/')
         cy.title().should('include', 'Brand Solutions – Hulu Advertising')
     })
-})
 
-describe('Case Studies Launch Test', function() {
-    it('Visits the Case Studies area', function() {
+    it('Opens the Case Studies area', function() {
         cy.visit('https://www.hulu.com/advertising/case-studies/')
         cy.title().should('include', 'Case Studies – Hulu Advertising')
     })
-})
 
-describe('Hulu Insights Launch Test', function() {
-    it('Visits the Hulu Insights area', function() {
+    it('Opens the Hulu Insights area', function() {
         cy.visit('https://www.hulu.com/advertising/insights/')
         cy.title().should('include', 'Hulu Insights – Hulu Advertising')
     })
-})
 
-describe('Hulu Originals Launch Test', function() {
-    it('Visits the Hulu Originals area', function() {
+    it('Opens the Hulu Originals area', function() {
         cy.visit('https://www.hulu.com/advertising/hulu-originals/')
         cy.title().should('include', 'Hulu Originals – Hulu Advertising')
     })
+
 })
+
+describe.only('Advertising Site Home Page Element Presences Tests', function() {
+
+    beforeEach(function() {
+        cy.visit(strSiteURL)
+        cy.title().should('include', 'Hulu Advertising')
+    })
+
+    context('Verify Hero Masthead', function () {
+
+        it('Verify Hero Masthead Section', function() {
+            cy
+            .get('#home-hero')
+            .should('have.class', 'hero')
+            .should('have.class', 'lg-header')
+        })
+
+        it('Verify Left Button', function() {
+            cy
+            .get('#home-hero > div > button:first')
+            .should('have.class', 'slick-prev')
+            .should('have.class', 'slick-arrow')
+        })
+
+        it('Verify Right Button', function() {
+            cy
+            .get('#home-hero > div > button:nth-of-type(2)')
+            .should('have.class', 'slick-next')
+            .should('have.class', 'slick-arrow')
+        })
+
+        it('Verify Tracker Dots', function() {
+            cy
+            .get('#home-hero > div > ul')
+            .should('have.class', 'slick-dots')
+        })
+    })
+})
+
+
+
+
